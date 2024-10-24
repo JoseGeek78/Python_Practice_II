4f251ce260e1cb7de411df4ae63072f8d2f9469d
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-10-24 08:25:26 +0200

    .

--- Practice.py
+++ Practice.py
@@ -1,8 +1,6 @@
 # frase_bienvenida = "Volviendo a lo básico"
 # print(frase_bienvenida)
 
-variable_1 = 'Hola Caracola!'
-
-variable_2 = variable_1
-
-print(variable_2)
+# variable_1 = 'Hola Caracola!'
+# variable_2 = variable_1
+# print(variable_2)
