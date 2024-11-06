3b2fa3af481e5f5cd1d9121f4397ed4ec8282950
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-11-06 15:04:38 +0100

    Updated

--- Practice.py
+++ Practice.py
@@ -4,8 +4,10 @@ numero_2 = 141
 numero_3 = "100"
 numero_4 = "300"
 
-resultado = numero_1 * numero_2
-print(resultado)
+resultado_1 = numero_1 * numero_2
+resultado_2 = numero_2 / numero_1
+print(resultado_1)
+print(resultado_2)
 # print(numero_1 + numero_2)
 # print(numero_3 + numero_4)
 
