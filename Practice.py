5de96b359c2fb4459b66fd6e985f904b1d8a2a21
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-12-02 14:53:14 +0100

    .

--- Practice.py
+++ Practice.py
@@ -1,5 +1,5 @@
 multiplicacion = 525823.559 * 8569.567
-print(multiplicacion)
+# print(multiplicacion)
 # print(round(multiplicacion))
 # print(round(multiplicacion, 2))
 # print(round(multiplicacion, 3))
