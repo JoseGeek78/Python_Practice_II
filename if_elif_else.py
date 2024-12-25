61320e9cc2e5421be0832fbfb78d5a2becb6018e
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-12-25 16:08:56 +0100

    .

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,7 +1,8 @@
-numero = 4
+numero = 7
 
 if numero > 7:
     print('El número es mayor que 7.')
 elif numero == 7:
+    print('El número es igual a 7.')
 else:
     print('El número es menor o igual a 7.')
