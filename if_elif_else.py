39f30f53298778e1db8ddeb41cd0dce220206fad
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-12-28 16:06:37 +0100

    .

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,24 +1,37 @@
-edad = int(input('Introduzca su edad:\n'))
 
-# Se deja la variable sin valor.
-respuesta = None
 
-# Se evalúa si el usuario es mayor de edad. Si lo es, accede 
-# a la compra de alcohol.
-if edad >= 18:
-    print('Es mayor de edad, puede comprar alcohol. ¿Cuál desea? Inroduzca un número de opción')
-    respuesta = input('1- ron.\n2- whisky\n183- ginebra.\n')
+
+
+# edad = int(input('Introduzca su edad:\n'))
+
+# # Se deja la variable sin valor.
+# respuesta = None
+
+# # Se evalúa si el usuario es mayor de edad. Si lo es, accede 
+# # a la compra de alcohol.
+# if edad >= 18:
+#     # Mensaje indicando que el usuario puede comprar alcohol.
+#     print('Es mayor de edad, puede comprar alcohol. ¿Cuál desea? Inroduzca un número de opción')
+#     # Se pide al usuario que seleccione una opción.
+#     respuesta = input('1- ron.\n2- whisky\n183- ginebra.\n')
     
-    if respuesta == '1':
-        print('Ha elegido comprar ron.')
-    elif respuesta == '2':
-        print('Ha elegido comprar whisky.')
-    elif respuesta == '3':
-        print('Ha elegido comprar ginebra.')
-    else:
-        print('Opción no válida.')
-else:
-    print('Es menor de edad, vuelva de aquí a un tiempo o no empiece con el alcohol.12')
+#     # Se evalúa la respuesta ingresada por el usuario.
+#     if respuesta == '1':
+#         # Mensaje para la opción de ron.
+#         print('Ha elegido comprar ron.')
+#     elif respuesta == '2':
+#         # Mensaje para la opción de whisky.
+#         print('Ha elegido comprar whisky.')
+#     elif respuesta == '3':
+#         # Mensaje para la opción de ginebra.
+#         print('Ha elegido comprar ginebra.')
+#     else:
+#         # Mensaje si la opción ingresada no es válida.
+#         print('Opción no válida.')
+# else:
+#     # Mensaje para menores de edad.
+#     print('Es menor de edad, vuelva de aquí a un tiempo o no empiece con el alcohol.')
+
 
 
 
