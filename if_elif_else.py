269e04b0c62a001bb5c9106208a14604c1315f10
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-12-31 15:39:42 +0100

    .

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,20 +1,23 @@
-error = input('Introduzca un código de error:\n')
 
-match error:
-    case '200':
-        print('Todo ok.')
-    case '301':
-        print('Movimiento permanente de la página.')
-    case '302':
-        print('Movimiento temporal de la página.')
-    case '404':
-        print('Página no encontrada.')
-    case '500':
-        print('Error interno del servidor.')
-    case '503':
-        print('Servicio no disponible.')
-    case defecto:
-        print('Error no disponible.')
+
+
+# error = input('Introduzca un código de error:\n')
+
+# match error:
+#     case '200':
+#         print('Todo ok.')
+#     case '301':
+#         print('Movimiento permanente de la página.')
+#     case '302':
+#         print('Movimiento temporal de la página.')
+#     case '404':
+#         print('Página no encontrada.')
+#     case '500':
+#         print('Error interno del servidor.')
+#     case '503':
+#         print('Servicio no disponible.')
+#     case defecto:
+#         print('Error no disponible.')
 
 # color = 'azul'
 # forma = 'cuadrado'
