c566fc44e9aedecefb1a53545dda6614aaa41993
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-01-11 08:18:42 +0100

    Updated

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -3,5 +3,5 @@ lista_colores = ['Rojo.', 'Azul.', 'Verde.', 'Amarillo.']
 print(lista_colores)
 print(lista_colores[0])
 print(lista_colores[2])
-print(lista_colores)
+print(lista_colores[3])
 print(f'El segundo elemento de la lista es: {lista_colores[1]}')
