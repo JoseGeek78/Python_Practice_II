bb316de3c75db34662ed5f37dd2e631da84797bc
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-01-12 14:59:36 +0100

    Updated

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -4,3 +4,9 @@
 # print(lista_colores[3])
 # print(f'El primer elemento de la lista es: {lista_colores[0]}')
 # print(f'El segundo elemento de la lista es: {lista_colores[1]}')
+
+lista = ['Redonda.', 'Cuadrada.', 'Triangular.']
+
+print(lista)
+print(lista[0])
+print(f'La figura tiene forma: {lista[1]}')
