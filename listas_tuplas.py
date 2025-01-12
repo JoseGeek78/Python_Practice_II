fc539c220892ccfe542ae51900e25d85a4f2b7b8
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-01-12 10:02:35 +0100

    .

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -1,6 +1,6 @@
 lista_colores = ['Rojo.', 'Azul.', 'Verde.', 'Amarillo.']
 
 print(lista_colores)
-print(lista_colores[3])
-print(f'El primer elemento de la lista es: {lista_colores[0]}')
-print(f'El segundo elemento de la lista es: {lista_colores[1]}')
+# print(lista_colores[3])
+# print(f'El primer elemento de la lista es: {lista_colores[0]}')
+# print(f'El segundo elemento de la lista es: {lista_colores[1]}')
