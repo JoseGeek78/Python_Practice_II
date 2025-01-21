cb4df086c5497013a91e9c19f84c912da97f000a
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-01-21 09:48:16 +0100

    .

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -1,4 +1,4 @@
-# lista_colores = ['Azul', 'Rojo', 'Rojo', 'Verde', 'Amarillo', 'Morado']
+lista_colores = ['Azul', 'Rojo', 'Rojo', 'Verde', 'Amarillo', 'Morado']
 
 # print(lista_colores.count('Rojo'))
 # print(lista_colores.index('Rojo'))
@@ -9,8 +9,11 @@
 
 # print(lista)
 
-# lista_numeros = [55, 10, 65, 3, 158, 6, 3, 45, 3, 986, 3]
+lista_numeros = [55, 10, 65, 3, 158, 6, 3, 45, 3, 986, 3]
 
+lista_colores.extend(lista_numeros)
+
+print(lista_colores)
 # lista_numeros.sort(reverse=True)
 
 # print(lista_numeros)
