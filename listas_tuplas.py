129c8c41e17f83bdc30cf08188dfc010c4aeedb8
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-01-23 08:42:12 +0100

    Updated

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -1,3 +1,8 @@
+lista_colores = ['rojo', 'azul', 'verde', 'amarillo']
+
+print(lista_colores[3][4])
+
+
 # lista_numeros = [10, 45, 55, 76]
 
 # print(f'El valor más pequeño en la lista es el {lista_numeros[0]}. El más grande, el {lista_numeros[3]}.')
