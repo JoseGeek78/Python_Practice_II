6cdb4df909152a8aceb0c0cf4cf2be1a2bfe43db
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-02-05 19:03:11 +0100

    Updated

--- for_while.py
+++ for_while.py
@@ -1,2 +1,7 @@
-for i in range(3,7):
+for i in range(3,8):
     print(f'El valor del bucle es {i}.')
+
+print('Fin del blucle.')
+
+# for i in range(3,7):
+#     print(f'El valor del bucle es {i}.')
