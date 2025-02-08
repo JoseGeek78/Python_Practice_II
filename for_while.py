8f65fc8b6acdeb8fe25d913ab6960e3cd5aae167
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-02-08 09:06:41 +0100

    .

--- for_while.py
+++ for_while.py
@@ -1,10 +1,13 @@
-colores = ['Rojo', 'Azul', 'Verde', 'Amarillo']
 
-print('---LISTADO DE COLORES---')
 
-for color in colores:
-    print(f'-{color}.')
 
+
+# colores = ['Rojo', 'Azul', 'Verde', 'Amarillo']
+
+# print('---LISTADO DE COLORES---')
+
+# for color in colores:
+#     print(f'-{color}.')
 
 # for i in range(3,13,3):
 #     print(f'El valor del bucle es: {i}.')
