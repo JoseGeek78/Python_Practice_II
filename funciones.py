6b05a48267748f1b82a13b1459c1bf2c8b5f5a3d
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-02-20 18:11:54 +0100

    Updated

--- funciones.py
+++ funciones.py
@@ -1,7 +1,8 @@
-def saludar(nombre):
+def saludar(nombre, edad):
     print(f'!Hola {nombre}!')
+    print(f'Tú tienes {edad} años.')
     
-saludar('Jose')
-saludar('Sergio')
-saludar('Fran')
-saludar('Vane')
+saludar('Jose', 46)
+saludar('Sergio', 26)
+saludar('Fran', 23)
+saludar('Vane', 37)
