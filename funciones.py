a1a7eb6ce4bcab4cdf1c325576cdc220ed94fc99
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-02-27 17:31:39 +0100

    Updated

--- funciones.py
+++ funciones.py
@@ -1,12 +1,20 @@
-colores = ['rojo', 'verde', 'amarillo']
+#colores = ['rojo', 'verde', 'amarillo']
 
-def añadir_color(color):
-    colores.insert(0, color)
+#def añadir_color(color):
+#    colores.insert(0, color)
     
-añadir_color(input('Escriba un color para añadirlo a la lista:\n'))
+#añadir_color(input('Escriba un color para añadirlo a la lista:\n'))
 
-print(colores)
+#print(colores)
 
+transporte = ['bicicleta', 'autobús', 'coche']
+
+def añadir_transport(transport):
+    transporte.insert(0, transport)
+    
+añadir_transport(input('Escriba un medio de transporte para añadirlo a la lista:\n'))
+
+print(transporte)
 
 #def suma(numero1,numero2):
 #    return numero1 + numero2
