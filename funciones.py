5ad18acfed8067932c53c104e0cd3581a0122edd
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-05 10:02:24 +0100

    .

--- funciones.py
+++ funciones.py
@@ -39,7 +39,17 @@ def calculadora():
             
             if opcion == '1':
                 print(f"Resultado: {suma(num1, num2)}")
-            elif
+            elif opcion == '2':
+                print(f"Resultado: {resta(num1, num2)}")
+            elif opcion == '3':
+                print(f"Resultado: {multiplicacion(num1, num2)}")
+            elif opcion == '4':
+                print(f"Resultado: {division(num1, num2)}")
+        else:
+            print("Opción no válida, inténtelo de nuevo.")
+            
+# Llamamos a la función principal para ejecutar la calculadora
+calculadora()
 
 
 
